78a4a285232a1a4578f535b10fd60b9c0f7f715a
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-08-28 13:01:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -170,7 +170,16 @@ function App() {
         {showNav ? 'Hide Filters' : 'Show Filters'}
       </button>
     )}
-    
+
+    <div style="position: relative; width: 100%; height: 0; padding-top: 56.2225%;
+     padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+     border-radius: 8px; will-change: transform;">
+      <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+        src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPFk1sFko&#x2F;75EaoJ3DdMworIiqCcMXHw&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+      </iframe>
+    </div>
+    <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGPFk1sFko&#x2F;75EaoJ3DdMworIiqCcMXHw&#x2F;view?utm_content=DAGPFk1sFko&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">Copy of Price Sign Template</a> by Jchen
+
       <div className="product-cards">
         <h1>Green Country ReStores' Recent Facebook Items</h1>
         <div className="product"> {

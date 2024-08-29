673d98c5db4c3150a00bd71df677db11b52f17d2
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-08-29 11:10:52 -0500

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -1,15 +1,15 @@
 export default function ProductItem(props) {   
   return (
     <div className="item">
-      <h2>{props.info["name"]}</h2>
+      /*<h2>{props.info["name"]}</h2>*/
       <img src={props.info["image_link"]}></img>
-      <ul>
+      /*<ul>
         <li>Product Type: {props.info["product_type"]}</li>
         <li>Processor: {props.info["processor"]}</li>
         <li>Release Time: {props.info["release_time"]}</li>
         <li>Note: {props.info["note"]}</li>
         <li>Unit Price: ${props.info["unit_price"]}</li>
-      </ul>
+      </ul>*/
       <button onClick={() => props.setStateOfParent(props.info["name"], props.info["unit_price"])}>
         {props.added === 1 ? "Remove" : "Calculate Total"}</button>
     </div>);

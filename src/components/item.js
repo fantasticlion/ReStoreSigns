0a659c4c5e51b0b7e502275fe5697756c2fa14c7
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-09-03 14:52:11 -0500

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -3,6 +3,8 @@ export default function ProductItem(props) {
     <div className="item">
       <img src={props.info["image_link"]}></img>
       <button onClick={() => props.setStateOfParent(props.info["name"], props.info["unit_price"])}>
-        {props.added === 1 ? "Remove" : "Calculate Total"}</button>
+        Add</button>
+      <button onClick={() => props.setStateOfParent(props.info["name"], props.info["unit_price"])}>
+        Remove</button>
     </div>);
 }

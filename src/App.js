b2c8e795ce858ef3381a30c1251b2d6037d415b5
fantasticlion <115042188+fantasticlion@users.noreply.github.com>
2024-09-04 17:11:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ function App() {
   
     if (action === 'add') {
       tempFavorites[name] = (tempFavorites[name] || 0) + 1;
-      setTotal((total + parseFloat(unit_price)).toFixed(2));
+      setTotal(total + parseFloat(unit_price));
     } else if (action === 'remove') {
       if (tempFavorites[name] > -1) {
         tempFavorites[name] -= 1;
-        setTotal((total - parseFloat(unit_price)).toFixed(2));
+        setTotal(total - parseFloat(unit_price));
       }
     }
-  
+    setTotal(total.toFixed(2));
     setFavorites(tempFavorites);
   };
 
